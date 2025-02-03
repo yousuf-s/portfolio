d1d98e21b5ab3576e00cfc1589455b4fd2c1446e
yousuf-s <s.yousuf1235@gmail.com>
2025-02-03 14:57:20 +0530

    Bootstrap config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,5 @@
-import pluginVue from 'eslint-plugin-vue'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginVue from "eslint-plugin-vue";
+import { defineConfigWithVueTs, vueTsConfigs } from "@vue/eslint-config-typescript";
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -9,16 +8,20 @@ import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default defineConfigWithVueTs(
   {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+    name: "app/files-to-lint",
+    files: ["**/*.{ts,mts,tsx,vue}"],
+    rules: {
+      semi: ["error", "always"], // Enforce semicolons
+      quotes: ["error", "double"], // Example: Force double quotes
+      "no-console": "warn", // Example: Warn on console.log
+    },
   },
 
   {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    name: "app/files-to-ignore",
+    ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
   },
 
-  pluginVue.configs['flat/essential'],
-  vueTsConfigs.recommended,
-  skipFormatting,
-)
+  pluginVue.configs["flat/essential"],
+  vueTsConfigs.recommended
+);
